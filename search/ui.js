ff83923f70c9ad42a1e0c322d712a0c7d407909f
periodico-caribe-brasil <joshuaortizbaco@utexas.edu>
2019-04-25 16:27:34 -0500

    fixed search

--- search/ui.js
+++ search/ui.js
@@ -6,7 +6,16 @@ $.getJSON("{{ '/search/index.json' | absolute_url }}", function(index_json) {
   window.store = index_json;
   index.saveDocument(false);
   index.setRef('lunr_index');
-  
+  index.addField('link');
+	index.addField('collection');
+	index.addField('artist');
+	index.addField('location');
+	index.addField('label');
+	index.addField('_date');
+	index.addField('object_type');
+	index.addField('current_location');
+	index.addField('pid');
+	index.addField('thumbnail');
   // add docs
   for (i in store){
     index.addDoc(store[i]);
@@ -21,7 +30,7 @@ $.getJSON("{{ '/search/index.json' | absolute_url }}", function(index_json) {
       var item = store[ref];
       var pid = item.pid;
       var label = item.label;
-      var meta = ``;
+      var meta = `${item.link} | ${item.collection} | ${item.artist}`;
       if ('thumbnail' in item) {
         var thumb = `<img class='sq-thumb-sm' src='{{ "" | absolute_url }}${item.thumbnail}'/>&nbsp;&nbsp;&nbsp;`
       }
